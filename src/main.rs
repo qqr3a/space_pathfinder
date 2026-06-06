3d2a5ee689e4a51d6d6590cb64c73e125bf61782
qqr3a <nitro5cy@outlook.com>
2026-06-06 17:52:50 +0100

    Fix: changed testing values to f64

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 mod body;
+
 use body::Body;
 use body::Vector2D;
 
 fn main() {
-    let earth = Body::new(2, Vector2D::new(2, 4));
+    let mut earth = Body::new(5.0, Vector2D::new(0.0, 0.0));
 
     earth.print();
+    for _n in 1..=10 {
+        earth.apply_force(Vector2D::new(1.0, 0.0));
+        earth.update(1.0);
+        earth.print();
+    }
 }
